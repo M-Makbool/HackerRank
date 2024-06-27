33beff40985da636784fadb0268eef71eb0cc79d
Mohammed Makbool <M.Makbool@outlook.sa>
2024-06-27 20:33:09 +0300

    27-6-2024

---
+++ C++/Strings/StringStream/c-tutorial-stringstream.cpp
@@ -0,0 +1,27 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+    stringstream ss(str);
+    vector<int> integers;
+    int num;
+    while ( ss ) {
+        ss >> num;
+        integers.push_back(num);
+        ss.ignore();
+    }
+    return integers;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
